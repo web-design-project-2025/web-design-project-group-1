635f8fa971234d9fc73a568659d30f3256b6ce20
h4shBrowns <fohe24kc@student.ju.se>
2025-04-29 17:40:32 +0200

    Make html file for showing categories page and more comments in main.js

--- main.js
+++ main.js
@@ -4,11 +4,13 @@
 const alcoholCategories = ['Vodka', 'Rum', 'Tequila'];
 
 async function getDrinkByIngredient (ingredient) {
+    // Guard clause warning if array is empty
     if(!ingredient) {
         console.warn('No ingredient provided to getDrinkByIngredient');
         return [];
     }
 
+    // Builds API request with encodeURIComponent to protect from special characters or spaces
     const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php';
     const query = `?i=${encodeURIComponent(ingredient)}`;
 
@@ -20,6 +22,7 @@ async function getDrinkByIngredient (ingredient) {
             console.error(`Failed to fetch drinks: ${res.status}`);
             return [];
         }
+        // Pulls only the drinks property from the returned object from the API
         const { drinks } = await res.json();
         return Array.isArray(drinks) ? drinks : [];
     } catch (err) {
@@ -28,10 +31,15 @@ async function getDrinkByIngredient (ingredient) {
     }
 }
 
-//Test the getDrinkByIngredient async function
+//Test the getDrinkByIngredient async function by logging categories in console
 async function testGetDrinkByIngredient() {
     const vodkaDrinks = await getDrinkByIngredient('Vodka');
+    const rumDrinks = await getDrinkByIngredient('Rum');
+    const tequilaDrinks = await getDrinkByIngredient('Tequila');
+
     console.log('Vodka drinks:', vodkaDrinks);
+    console.log('Rum Drinks:', rumDrinks);
+    console.log('Tequila Drinks', tequilaDrinks);
 }
 
 testGetDrinkByIngredient();
